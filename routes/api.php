c5f0ca62342e214b01dcbae5c20414172669d625
crcms <crcms@crcms.cn>
2018-04-06 09:41:24 +0000

    routes

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,9 @@ Route::prefix('api/v1')->middleware(['api'])->group(function () {
         Route::post('register','RegisterController@register');
         Route::post('login','LoginController@login');
     });
+
+    Route::prefix('manage')->namespace('CrCms\User\Http\Controllers\Api\Manage')->group(function(){
+        Route::resource('users','UserController');
+    });
 });
 
