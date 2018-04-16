3563f5605078ec66fa18f1fe78b960f14ca18a91
crcms <crcms@crcms.cn>
2018-04-16 14:28:48 +0000

    user search

--- src/Repositories/Magic/UserMagic.php
+++ src/Repositories/Magic/UserMagic.php
@@ -9,6 +9,7 @@
 
 namespace CrCms\User\Repositories\Magic;
 
+use function CrCms\Foundation\App\Helpers\date_to_timestamp;
 use CrCms\Repository\AbstractMagic;
 use CrCms\Repository\Contracts\QueryRelate;
 
@@ -23,9 +24,38 @@ class UserMagic extends AbstractMagic
      * @param string $name
      * @return QueryRelate
      */
-    public function byName(QueryRelate $queryRelate,string $name): QueryRelate
+    public function byName(QueryRelate $queryRelate, string $name): QueryRelate
     {
-        return $queryRelate->where('name',$name);
+        return $queryRelate->where('name', $name);
     }
 
+    /**
+     * @param QueryRelate $queryRelate
+     * @param string $email
+     * @return QueryRelate
+     */
+    public function byEmail(QueryRelate $queryRelate, string $email): QueryRelate
+    {
+        return $queryRelate->where('email', $email);
+    }
+
+    /**
+     * @param QueryRelate $queryRelate
+     * @param string $tel
+     * @return QueryRelate
+     */
+    public function byTel(QueryRelate $queryRelate, string $tel): QueryRelate
+    {
+        return $queryRelate->where('tel', $tel);
+    }
+
+    /**
+     * @param QueryRelate $queryRelate
+     * @param array $dateTimes
+     * @return QueryRelate
+     */
+    public function byCreatedAt(QueryRelate $queryRelate, array $dateTimes): QueryRelate
+    {
+        return $queryRelate->whereBetween('created_at', date_to_timestamp($dateTimes));
+    }
 }
