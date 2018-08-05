4b6710a7f4a7547ac19421e8ba45b2d6b0957507
crcms <crcms@crcms.cn>
2018-08-05 21:52:53 +0800

    debug

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('api/v1')->middleware(['api'])->group(function () {
-    Route::prefix('auth')->middleware([])->namespace('CrCms\User\Http\Controllers\Api\Auth')->group(function () {
-        Route::post('register', 'RegisterController@register');
-        Route::post('login', 'LoginController@login')->name('auth.login');
-        Route::get('logout', 'LoginController@logout')->name('auth.logout');
-        Route::post('forget-password', 'ForgotPasswordController@sendResetLinkEmail');
-        Route::post('reset-password-url', 'ForgotPasswordController@postResetPasswordUrl')->name('user.auth.forget_password.reset_password_url');
-        Route::post('reset-password', 'ResetPasswordController@reset')->name('user.auth.reset_password.reset');
-        Route::middleware('signed')->get('behavior-auth/{behavior_id}', 'BehaviorAuthController@getCertification')->name('user.auth.behavior_auth.get');
+Route::middleware(['api'])->group(function () {
+    Route::domain('api.user.crcms.local')->namespace('CrCms\User\Http\Controllers\Api')
+        ->middleware('auth:api')
+        ->group(function(){
+        Route::get('/','UserController@getShow')->name('test.index');
     });
 
     Route::prefix('manage')->namespace('CrCms\User\Http\Controllers\Api\Manage')->group(function () {
---
+++ src/Http/Controllers/Api/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @author simon <simon@crcms.cn>
+ * @datetime 2018-08-05 19:27
+ * @link http://crcms.cn/
+ * @copyright Copyright &copy; 2018 Rights Reserved CRCMS
+ */
+
+namespace CrCms\User\Http\Controllers\Api;
+
+use CrCms\Foundation\App\Http\Controllers\Controller;
+
+/**
+ * Class UserController
+ * @package CrCms\User\Http\Controllers\Api
+ */
+class UserController extends Controller
+{
+
+    public function getShow()
+    {
+        return 123;
+    }
+
+}
