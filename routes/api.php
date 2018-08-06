25038a23f7fd932d0b2be180a47e2b520a3b5bc5
crcms <crcms@crcms.cn>
2018-08-06 21:21:50 +0800

    Routing

--- routes/api.php
+++ routes/api.php
@@ -2,23 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['api'])->group(function () {
-    Route::prefix('api/v1')->namespace('CrCms\User\Http\Controllers\Api')
-        ->middleware('auth:api')
-        ->group(function(){
+Route::prefix('api/v1')->middleware(['api'])->namespace('CrCms\User\Http\Controllers\Api')->group(function () {
+
+    Route::post('passport','CrCms\User\Http\Controllers\Api\PassportController@postPassport')->name('user.passport.post');
+
+    Route::middleware('auth:api')->group(function(){
         Route::get('/','UserController@getShow')->name('test.index');
-
     });
 
-    Route::any('passport','CrCms\User\Http\Controllers\Api\PassportController@check');
-
-    Route::prefix('manage')->namespace('CrCms\User\Http\Controllers\Api\Manage')->group(function () {
-        Route::apiResource('users', 'UserController')->names([
-            'index' => 'user.users.index',
-            'store' => 'user.users.store',
-            'update' => 'user.users.update',
-            'destroy' => 'user.users.destroy',
-        ])->except(['show']);
-    });
 });
 
+// manage
+Route::prefix('api/v1/manage')->middleware(['api'])->namespace('CrCms\User\Http\Controllers\Api\Manage')->group(function () {
+
+    Route::apiResource('users', 'UserController')->names([
+        'index' => 'user.manage.users.index',
+        'store' => 'user.manage.users.store',
+        'update' => 'user.manage.users.update',
+        'destroy' => 'user.manage.users.destroy',
+    ])->except(['show']);
+
+});
