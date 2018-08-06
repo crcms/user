5eb463da809b2c434b9aa5794ee8c78e0fee80d3
crcms <crcms@crcms.cn>
2018-08-06 21:08:30 +0800

    Remove auth config

--- src/Providers/UserServiceProvider.php
+++ src/Providers/UserServiceProvider.php
@@ -3,17 +3,7 @@
 namespace CrCms\User\Providers;
 
 use CrCms\Foundation\App\Providers\ModuleServiceProvider;
-use CrCms\User\Events\BehaviorCreatedEvent;
-use CrCms\User\Events\ForgetPasswordEvent;
-use CrCms\User\Events\RegisteredEvent;
-use CrCms\User\Listeners\BehaviorCreatedListener;
-use CrCms\User\Listeners\ForgetPasswordMailListener;
-use CrCms\User\Listeners\RegisterMailListener;
-use CrCms\User\Listeners\Repositories\UserBehaviorListener;
-use CrCms\User\Listeners\Repositories\UserListener;
-use CrCms\User\Repositories\UserBehaviorRepository;
-use CrCms\User\Repositories\UserRepository;
-use Illuminate\Support\Facades\Event;
+use CrCms\User\Models\UserModel;
 use Tymon\JWTAuth\Providers\LaravelServiceProvider;
 
 /**
@@ -39,8 +29,9 @@ class UserServiceProvider extends ModuleServiceProvider
     {
         parent::boot();
 
+        config(['auth.providers.users.model' => UserModel::class]);
+
         $this->publishes([
-            $this->basePath . 'config/auth.php' => config_path('auth.php'),
             $this->basePath . 'config/config.php' => config_path("{$this->name}.php"),
             $this->basePath . 'resources/lang' => resource_path("lang/vendor/{$this->name}"),
         ]);
@@ -54,10 +45,6 @@ class UserServiceProvider extends ModuleServiceProvider
     public function register(): void
     {
         parent::register();
-
-        $this->mergeConfigFrom(
-            $this->basePath . 'config/auth.php', 'auth'
-        );
 
         $this->app->register(\Illuminate\Auth\AuthServiceProvider::class);
         $this->app->register(\Illuminate\Auth\Passwords\PasswordResetServiceProvider::class);
